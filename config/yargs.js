1e61a95473c5b3d824b99f5b2671af02e0dff381
jav_dev <anayaacostajavier@gmail.com>
2023-03-26 14:46:30 -0400

    Primer Commit

---
+++ config/yargs.js
@@ -0,0 +1,37 @@
+
+const argv = require('yargs')
+                    .option('b',{
+                        alias: 'base',
+                        type: 'number',
+                        demandOption: true,
+                        default:false,
+                        describe: "Esta es la base de la tabla de multiplicar"
+
+                    })
+                    .option('l',{
+                        alias: 'listar',
+                        type: 'boolean',
+                        default: false,
+                        describe: "Muestra la tabla en consola"
+                    })
+                    .option('h',{
+                        alias: 'hasta',
+                        type: 'number',
+                        default: 10,
+                        describe: 'Este es el numero hasta el cuela se multiplica'
+                    })
+                    .check( (argv, options) =>{
+                        if( isNaN( argv.b)){
+                            throw 'La base tiene que ser un numero'
+                        }
+                        return true
+                    })
+                    .check( (argv, options) => {
+                        if( isNaN(argv.h)){
+                            throw 'El limite tiene que ser un numero'
+                        }
+                        return true
+                    })
+                    .argv;
+
+module.exports =  argv ;
